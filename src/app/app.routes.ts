89d7b3b85706fd01637011d231e068b6115e1885
Ndumiso <accounts@tybo.co.za>
2025-08-26 04:31:31 +0200

    feat: restructure routing to include HomeComponent and enhance landing page with LandingHeroComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,47 +1,45 @@
 import { Routes } from '@angular/router';
-import { AdminComponent } from './components/admin/admin.component';
-import { UserManageComponent } from './components/admin/user-components/user-manage/user-manage.component';
-import { UserListComponent } from './components/admin/user-components/user-list/user-list.component';
 import { RegisterComponent } from './components/accounts/register/register.component';
 import { LoginComponent } from './components/accounts/login/login.component';
 import { ProfileComponent } from './components/accounts/profile/profile.component';
 import { EditProfileComponent } from './components/accounts/edit-profile/edit-profile.component';
 import { LogoutComponent } from './logout/logout.component';
-import { ApplicationsComponent } from './components/admin/applications/applications.component';
-import { SettingsComponent } from './settings/settings.component';
-import { AdminDashboardComponent } from './app/admin/dashboard/admin-dashboard.component';
-import { ReportsPageComponent } from './components/admin/reports/reports-page.component';
-import { BankingDetailsPageComponent } from './components/admin/banking/banking-details-page.component';
 import { adminRoutes } from './routes/admin.routes';
 import { LandingComponent } from './components/landing/landing.component';
-import { LandingGoodLogicComponent } from './components/home/landing/landing.good.logic';
-import { LandingHeroComponent } from './components/home/landing/landing.component';
 import { ApplyPageComponent } from './components/home/apply/apply';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingHeroComponent },
-  // { path: '', component: LandingGoodLogicComponent },
-  { path: 'apply', component: ApplyPageComponent },
   {
-    path: 'application/:id/done',
-    loadComponent: () =>
-      import('./components/home/thank-you/thank-you.component').then(
-        (m) => m.ThankYouComponent
-      ),
+    path: '',
+    component: HomeComponent,
+
+    children: [
+      { path: '', component: LandingComponent },
+      { path: 'apply', component: ApplyPageComponent },
+      {
+        path: 'application/:id/done',
+        loadComponent: () =>
+          import('./components/home/thank-you/thank-you.component').then(
+            (m) => m.ThankYouComponent
+          ),
+      },
+      {
+        path: 'status',
+        loadComponent: () =>
+          import('./components/home/status/status.component').then(
+            (m) => m.StatusComponent
+          ),
+      },
+      { path: 'logout', component: LogoutComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'accounts/login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'accounts/register', component: RegisterComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'edit-profile', component: EditProfileComponent },
+    ],
   },
-  {
-    path: 'status',
-    loadComponent: () =>
-      import('./components/home/status/status.component').then(
-        (m) => m.StatusComponent
-      ),
-  },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'accounts/login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'accounts/register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'edit-profile', component: EditProfileComponent },
+
   ...adminRoutes,
 ];
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { HeroSectionComponent } from '../home/hero-section/hero-section.component';
 import { WhyChooseUsComponent } from '../home/why-choose-us/why-choose-us.component';
 import { TestimonialsComponent } from '../home/testimonials/testimonials.component';
 import { CtaComponent } from '../home/cta/cta.component';
+import { LandingHeroComponent } from "../home/landing/landing.component";
 
 @Component({
   selector: 'app-landing',
   imports: [
-    HeroSectionComponent,
     WhyChooseUsComponent,
     TestimonialsComponent,
     CtaComponent,
-  ],
+    LandingHeroComponent
+],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
